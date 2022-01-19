53293f89e766e076f1cc93071fa1ecd753a205e2
Caique Nerivan <caiquenerivansantos@gmail.com>
2022-01-18 23:37:59 -0300

    começo do app

---
+++ my-app/src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Header(){
+    return(
+        <>
+            <div class=""></div>
+            <div class="main"> 
+                <div class="container">
+                    <h2>NOVOS PRODUTOS</h2>
+                    <h1>
+                        INSTRUMENTOS PROFISSIONAIS
+                    </h1>
+                    <h3>CONFIRA</h3>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Header; 
